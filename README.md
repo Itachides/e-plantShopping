bdb4cbeb77a4487eb38db319c7840a3afded533a
Vikas <vikasgowda@omniforge.in>
2025-12-14 18:14:28 +0530

    import from others

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# coding-project-template
+# e-planting shopping 
+This repo contains the updated code as per the requirements of the course.
--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,30 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      console.log("ACTION:", action);
+      console.log("Before:", state.items);
+
+      const { name }  =  action.payload;
+      const existingItem = state.items.find(item => item.name === name);
+      if(existingItem) {
+        existingItem.quantity++;
+      } else {
+        state.items.push({ ...action.payload, quantity: 1 });
+      }
+        console.log("After:", state.items);
     },
     removeItem: (state, action) => {
+      const { name } = action.payload;
+      state.items = state.items.filter(item => item.name !== name);
+       console.log("removeItem:", state.items);
     },
     updateQuantity: (state, action) => {
-
-    
+      const { name, quantity } = action.payload;
+      const itemUppdate = state.items.find(item => item.name === name);
+      if(itemUppdate) {
+        itemUppdate.quantity = quantity;
+      }
+       console.log("updateQuantity:", state.items);
     },
   },
 });
